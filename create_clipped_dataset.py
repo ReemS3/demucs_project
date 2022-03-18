91c51e83d9104adbfcfcd33707cbb23efa606370
Reem Farah <reem.farah@visable.com>
2022-03-18 13:20:21 +0100

    modify create_clipped_dataset.py

--- create_clipped_dataset.py
+++ create_clipped_dataset.py
@@ -1,40 +1,46 @@
-from moviepy.editor import *
+from pydub import AudioSegment
 import tensorflow as tf
 import tqdm
 import os
 
-ORIGINAL_DATASET_ROOT = "./data/musdb18"
-ORIGINAL_TRAIN_DATASET_ROOT = "./data/musdb18/train/"
-ORIGINAL_TEST_DATASET_ROOT = "./data/musdb18/test/"
+ORIGINAL_DATASET_ROOT = "./data/musdb18hq"
+ORIGINAL_TRAIN_DATASET_ROOT = "./data/musdb18hq/train/"
+ORIGINAL_TEST_DATASET_ROOT = "./data/musdb18hq/test/"
 
 
-def decode_sources(source_dir):
+def decode(source_dir):
     """Decodes sources the sources are in following order: bass, drums, other, vocals."""
-    tracks_labels = []
+    mixture, sources_ls = [], []
     for track in tqdm.tqdm(os.listdir(source_dir)):
-        sources = []
-        if "DS_Store" not in track:
-            for source in tqdm.tqdm(os.listdir(source_dir+track)):
-                if "piano" not in source and "DS_Store" not in source:
-                    raw_audio = tf.io.read_file(source_dir + track+'/'+source)
-                    decoded_source, _ = tf.audio.decode_wav(
-                        contents=raw_audio, desired_samples=10000, desired_channels=2)
-                    sources.append(decoded_source)
-            tracks_labels.append(sources)
-    return tracks_labels
+        sources_track = []
+        for source in os.listdir(source_dir+track):
+            raw_audio = tf.io.read_file(source_dir + track+'/'+source)
+            decoded_source, _ = tf.audio.decode_wav(
+                contents=raw_audio, desired_samples=1323000, desired_channels=2)
+            if "mixture" in source:
+                mixture.append(decoded_source)
+            else:
+                sources_track.append(decoded_source)
+        sources_ls.append(sources_track)
+    return mixture, sources_ls
 
 
 def clip_and_convert_toWAV(source_dir, target_dir):
     """Clip the audio track and create a new track containing only the first 30 seconds."""
+    new_length = 30000
     for test_track in tqdm.tqdm(os.listdir(source_dir)):
         track_path = source_dir + test_track
-        audio = VideoFileClip(track_path)
-        if audio.duration > 30:
-            new_audio = audio.set_duration(30)
-        else:
-            new_audio = audio
-        new_audio_name = target_dir + test_track[:-3] + "wav"
-        new_audio.audio.write_audiofile(new_audio_name)
+        os.mkdir(target_dir + "/" + test_track)
+        print("Create a folder for this track: ", test_track)
+        for source in os.listdir(track_path):
+            source_path = track_path + "/" + source
+            audio = AudioSegment.from_wav(source_path)
+            if len(audio) > new_length:
+                new_audio = audio[:30000]
+            else:
+                new_audio = audio
+            new_audio_name = target_dir + test_track + "/" + source
+            new_audio.export(new_audio_name, format="wav")
 
 
 def make_new_folder(folder_name):
@@ -45,47 +51,32 @@ def make_new_folder(folder_name):
         print("Folder already exists.")
 
 
-def decode(folder_name):
-    """Decode the WAV files in the given folder."""
-    decoded_data = []
-    for track in tqdm.tqdm(os.listdir(folder_name)):
-        if "DS_Store" not in track:
-            raw_audio = tf.io.read_file(folder_name + track)
-            decoded_track, _ = tf.audio.decode_wav(
-                contents=raw_audio, desired_samples=10000, desired_channels=2)
-            decoded_data.append(decoded_track)
-    return decoded_data
-
-
 def main():
     parent_path = "./data/musdb18_clipped"
     train_folder_path = parent_path + "/train/"
     test_folder_path = parent_path + "/test/"
 
-    parent_labels_path = "./data/musdb18_clipped_separated"
-    train_labels_folder_path = parent_labels_path + "/train/"
-    test_labels_folder_path = parent_labels_path + "/test/"
-
     make_new_folder(parent_path)
     make_new_folder(train_folder_path)
     make_new_folder(test_folder_path)
 
+    # if you already have clipped the tracks, comment out the two following lines
     clip_and_convert_toWAV(ORIGINAL_TRAIN_DATASET_ROOT, train_folder_path)
     clip_and_convert_toWAV(ORIGINAL_TEST_DATASET_ROOT, test_folder_path)
 
-    train_labels = decode_sources(train_labels_folder_path)
-    test_labels = decode_sources(test_labels_folder_path)
+    train_mixture, train_sources = decode(
+        train_folder_path)
 
-    train_ds = decode(train_folder_path)
-    test_ds = decode(test_folder_path)
+    test_mixture, test_sources = decode(
+        test_folder_path)
 
     train_tfds = (tf.data.Dataset.from_tensor_slices((
-        tf.cast(train_ds, tf.float32),
-        tf.cast(train_labels, tf.float32))))
+        tf.cast(train_mixture, tf.float32),
+        tf.cast(train_sources, tf.float32))))
 
     test_tfds = (tf.data.Dataset.from_tensor_slices((
-        tf.cast(test_ds, tf.float32),
-        tf.cast(test_labels, tf.float32))))
+        tf.cast(test_mixture, tf.float32),
+        tf.cast(test_sources, tf.float32))))
 
     tf.data.experimental.save(train_tfds, "./dataset_train")
     tf.data.experimental.save(test_tfds, "./dataset_test")
